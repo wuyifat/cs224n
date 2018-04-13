c5cca04f819adc4564cbb46bd34b2f9391c0c8b3
Yi Wu <wuyifat@yeah.net>
2018-04-12 20:52:44 -0500

    finish all hw

--- assignment3/q1_window.py
+++ assignment3/q1_window.py
@@ -163,7 +163,7 @@ class WindowModel(NERModel):
         ### YOUR CODE HERE (~5-10 lines)
         feed_dict = {self.input_placeholder: inputs_batch,
                      self.dropout_placeholder: dropout}
-        if labels_batch:
+        if labels_batch is not None:
             feed_dict[self.labels_placeholder] = labels_batch
          
         ### END YOUR CODE
